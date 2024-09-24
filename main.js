d7fa60af4275dd72c22840a2ebf1894e9ae52964
Jonathan Perkins <jperkin4@uab.edu>
2024-09-24 00:28:41 -0500

    Creating background and first object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
 
-const scene = new THREE.scene();
+const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -13,8 +13,36 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
+
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0x341a00 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+
+
 const skyTexture = new THREE.TextureLoader().load('./src/pink-sky.jpg');
 scene.background = skyTexture;
+
+
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.05;
+  torus.rotation.y += 0;
+  torus.rotation.z += 0;
+
+  // controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
 
 /*
 document.querySelector('#app').innerHTML = `
