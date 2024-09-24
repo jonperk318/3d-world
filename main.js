389f305399485d2695a12a4cd31e5626e4b9ec9c
Jonathan Perkins <jperkin4@uab.edu>
2024-09-23 23:57:46 -0500

    Trying to create scene

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 import * as THREE from 'three';
 
+const scene = new THREE.scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const skyTexture = new THREE.TextureLoader().load('./src/pink-sky.jpg');
+scene.background = skyTexture;
+
+/*
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello World</h1>
@@ -9,3 +24,4 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+*/
